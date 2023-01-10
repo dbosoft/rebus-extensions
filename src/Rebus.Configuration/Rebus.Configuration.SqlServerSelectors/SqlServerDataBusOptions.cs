ecf32c73cc9deb86dae3cca80a0ba140624de78d
Frank Wagner <frank2568@gmail.com>
2023-01-10 15:51:37 +0100

    added sql server datbus storage

---
+++ src/Rebus.Configuration/Rebus.Configuration.SqlServerSelectors/SqlServerDataBusOptions.cs
@@ -0,0 +1,8 @@
+﻿namespace Dbosoft.Rebus.Configuration;
+
+public class SqlServerDataBusOptions
+{
+    public string TableName { get; set; }
+
+    public bool AutomaticallyCreateTables { get; set; }
+}
---
+++ src/Rebus.Configuration/Rebus.Configuration.SqlServerSelectors/SqlServerDataBusSelector.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Rebus.Config;
+using Rebus.DataBus;
+
+namespace Dbosoft.Rebus.Configuration;
+
+public class SqlServerDataBusSelector : SqlServerSelectorBase<IDataBusStorage>
+{
+    private readonly IOptions<SqlServerDataBusOptions> _options;
+
+    public SqlServerDataBusSelector(IOptions<SqlServerDataBusOptions> options, IConfiguration configuration, ILogger log)
+        : base(configuration, log)
+    {
+        _options = options;
+    }
+
+    public override string ConfigurationName => "store";
+    protected override void ConfigureSqlServer(StandardConfigurer<IDataBusStorage> configurer, string connectionString)
+    {
+        configurer.StoreInSqlServer(connectionString,
+            _options.Value.TableName, _options.Value.AutomaticallyCreateTables);
+    }
+}
