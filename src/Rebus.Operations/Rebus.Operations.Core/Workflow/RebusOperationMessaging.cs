9ec99067d20fbeb6a88da6d0f361760a2daaefa5
Frank Wagner <frank2568@gmail.com>
2023-07-26 15:04:35 +0200

    removed artifacts

--- src/Rebus.Operations/Rebus.Operations.Core/Workflow/RebusOperationMessaging.cs
+++ src/Rebus.Operations/Rebus.Operations.Core/Workflow/RebusOperationMessaging.cs
@@ -4,7 +4,6 @@ using System.Threading.Tasks;
 using Dbosoft.Rebus.Operations.Commands;
 using Dbosoft.Rebus.Operations.Events;
 using Rebus.Bus;
-using Rebus.Transport;
 
 namespace Dbosoft.Rebus.Operations.Workflow;
 
@@ -59,49 +58,4 @@ public class RebusOperationMessaging : IOperationMessaging
     public IOperationTaskDispatcher TaskDispatcher { get; }
     
     
-    public Task FailTask(IOperationTaskMessage message, string errorMessage, IDictionary<string,string>? additionalHeaders = null)
-    {
-        return FailTask(message, new ErrorData { ErrorMessage = errorMessage }, additionalHeaders);
-    }
-
-    public Task FailTask(IOperationTaskMessage message, ErrorData error, IDictionary<string,string>? additionalHeaders = null)
-    {
-        return _bus.SendWorkflowEvent(_options,
-            OperationTaskStatusEvent.Failed(
-                message.OperationId, message.InitiatingTaskId,
-                message.TaskId, error),additionalHeaders );
-    }
-
-
-    public Task CompleteTask(IOperationTaskMessage message, IDictionary<string,string>? additionalHeaders = null)
-    {
-        return _bus.SendWorkflowEvent(_options,
-            OperationTaskStatusEvent.Completed(
-                message.OperationId, message.InitiatingTaskId, message.TaskId), additionalHeaders);
-    }
-
-    public Task CompleteTask(IOperationTaskMessage message, object responseMessage, IDictionary<string,string>? additionalHeaders = null)
-    {
-        return _bus.SendWorkflowEvent(_options,
-            OperationTaskStatusEvent.Completed(
-                message.OperationId, message.InitiatingTaskId, message.TaskId, responseMessage), additionalHeaders);
-    }
-
-
-    public async Task ProgressMessage(IOperationTaskMessage message, object data, IDictionary<string,string>? additionalHeaders = null)
-    {
-        using var scope = new RebusTransactionScope();
-        
-        await _bus.SendWorkflowEvent(_options, new OperationTaskProgressEvent
-        {
-            Id = Guid.NewGuid(),
-            OperationId = message.OperationId,
-            TaskId = message.TaskId,
-            Data = data,
-            Timestamp = DateTimeOffset.UtcNow
-        }, additionalHeaders).ConfigureAwait(false);
-
-        // commit it like this
-        await scope.CompleteAsync().ConfigureAwait(false);
-    }
 }
