04bdcf7a5f35779048bd0bc22eaa1b8e9a5bec35
Christopher Mann <christopher.mann@dbosoft.eu>
2024-04-05 15:35:23 +0200

    Locking for operations

---
+++ src/Rebus.Operations/Rebus.Operations.Core/Workflow/OperationExclusiveAccessStep.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Dbosoft.Rebus.Operations.Commands;
+using Dbosoft.Rebus.Operations.Events;
+using Rebus.Messages;
+using Rebus.Pipeline;
+
+namespace Dbosoft.Rebus.Operations.Workflow;
+
+public class OperationExclusiveAccessStep(int numberOfBuckets, CancellationToken cancellationToken)
+    : IIncomingStep, IDisposable
+{
+    // This code is based on Rebus's SemaphoreSlimExclusiveSagaAccessIncomingStep
+
+    private readonly SemaphoreSlim[] _locks = Enumerable.Range(0, numberOfBuckets)
+        .Select(n => new SemaphoreSlim(1, 1))
+        .ToArray();
+
+    public Task Process(IncomingStepContext context, Func<Task> next)
+    {
+        var message = context.Load<Message>() ?? throw new ArgumentException(
+            "The deserialized message is missing. The pipeline is most likely ordered incorrectly.",
+            nameof(context));
+
+        return message.Body switch
+        {
+            IOperationTaskMessage otm and (CreateNewOperationTaskCommand or OperationTaskAcceptedEvent or OperationTaskStatusEvent) =>
+                RunWithLock(otm.OperationId, next),
+            OperationTaskProgressEvent otpe =>
+                RunWithLock(otpe.OperationId, next),
+            _ => next()
+        };
+    }
+
+    private async Task RunWithLock(Guid operationId, Func<Task> action)
+    {
+        await AcquireLockAsync(operationId);
+        try
+        {
+            await action();
+        }
+        finally
+        {
+            ReleaseLock(operationId);
+        }
+    }
+
+    private async Task AcquireLockAsync(Guid id)
+    {
+        var lockId = Math.Abs(id.GetHashCode() % numberOfBuckets);
+
+        await _locks[lockId].WaitAsync(cancellationToken);
+    }
+
+    private void ReleaseLock(Guid id)
+    {
+        var lockId = Math.Abs(id.GetHashCode() % numberOfBuckets);
+
+        _locks[lockId].Release();
+    }
+
+    private bool _disposed;
+
+    public void Dispose()
+    {
+        if (_disposed) return;
+
+        try
+        {
+            foreach (var disposable in _locks)
+            {
+                disposable.Dispose();
+            }
+        }
+        finally
+        {
+            _disposed = true;
+        }
+    }
+}
---
+++ src/Rebus.Operations/Rebus.Operations.Core/Workflow/OptionsConfigurerExtensions.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using Rebus.Config;
+using Rebus.Pipeline;
+using Rebus.Pipeline.Receive;
+
+namespace Dbosoft.Rebus.Operations.Workflow;
+
+public static class OptionsConfigurerExtensions
+{
+    /// <summary>
+    /// Enforces exclusive access when updating <see cref="IOperation"/>
+    /// or <see cref="IOperationTask"/>.
+    /// </summary>
+    /// <remarks>
+    /// It is very much recommended to enable this feature. Otherwise, any implementations
+    /// of <see cref="IOperationManager"/> or <see cref="IOperationTaskManager"/> must be able
+    /// to handle concurrent updates. There is a good change that not activating this
+    /// feature will cause hard to find concurrency bugs.
+    /// </remarks>
+    public static void EnableOperationExclusiveAccess(
+        this OptionsConfigurer optionsConfigurer,
+        int numberOfBuckets = 1000)
+    {
+        optionsConfigurer.Decorate<IPipeline>(c =>
+        {
+            var pipeline = c.Get<IPipeline>();
+            var cancellationToken = c.Get<CancellationToken>();
+            var step = new OperationExclusiveAccessStep(numberOfBuckets, cancellationToken);
+
+            // The step must run as early as possible to ensure proper protection.
+            // Hence, we attach directly after the message deserialization.
+            return new PipelineStepInjector(pipeline)
+                .OnReceive(step, PipelineRelativePosition.After, typeof(DeserializeIncomingMessageStep));
+        });
+    }
+}
