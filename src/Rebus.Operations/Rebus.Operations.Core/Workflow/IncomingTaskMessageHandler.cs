b924e285c3d8b888782e9d09b8a6961f8fc9b6d4
Frank Wagner <frank2568@gmail.com>
2024-04-12 14:19:46 +0200

    async processing of incoming message

--- src/Rebus.Operations/Rebus.Operations.Core/Workflow/IncomingTaskMessageHandler.cs
+++ src/Rebus.Operations/Rebus.Operations.Core/Workflow/IncomingTaskMessageHandler.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Dbosoft.Rebus.Operations.Commands;
 using Dbosoft.Rebus.Operations.Events;
@@ -6,6 +7,7 @@ using Microsoft.Extensions.Logging;
 using Rebus.Bus;
 using Rebus.Handlers;
 using Rebus.Pipeline;
+using Rebus.Transport;
 
 
 namespace Dbosoft.Rebus.Operations.Workflow
@@ -22,19 +24,26 @@ namespace Dbosoft.Rebus.Operations.Workflow
             _messageEnricher = messageEnricher;
         }
 
+        private static async void Resubmit(
+            IBus bus,
+            OperationTaskSystemMessage<T> taskMessage, IDictionary<string, string>? headers)
+        {
+            using var scope = new RebusTransactionScope();
+            await bus.SendLocal(new OperationTask<T>(taskMessage.Message, 
+                    taskMessage.OperationId, taskMessage.InitiatingTaskId,
+                    taskMessage.TaskId, taskMessage.Created)
+                , headers
+            ).ConfigureAwait(false);
+
+            await scope.CompleteAsync().ConfigureAwait(false);
+        }
+
         public async Task Handle(OperationTaskSystemMessage<T> taskMessage)
         {
             if(taskMessage.Message==null)
                 throw new InvalidOperationException($"Operation Workflow {taskMessage.OperationId}/{taskMessage.TaskId}: missing command message");
 
             var headers = _messageEnricher.EnrichHeadersFromIncomingSystemMessage(taskMessage, MessageContext.Current.Headers);
-            await _bus.SendLocal(new OperationTask<T>(taskMessage.Message,  taskMessage.OperationId,taskMessage.InitiatingTaskId, 
-                    taskMessage.TaskId, taskMessage.Created)
-            , headers
-            ).ConfigureAwait(false);
-
-            _logger.LogTrace($"Accepted incoming operation message. Operation id: '{taskMessage.OperationId}'");
-
             var reply = new OperationTaskAcceptedEvent
             {
                 OperationId = taskMessage.OperationId,
@@ -45,6 +54,9 @@ namespace Dbosoft.Rebus.Operations.Workflow
             };
 
             await _bus.Reply(reply).ConfigureAwait(false);
+            _logger.LogTrace($"Accepted incoming operation message. Operation id: '{taskMessage.OperationId}'");
+
+            Resubmit(_bus, taskMessage, headers);
         }
     }
 }
